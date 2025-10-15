149b1f018d129209ba6099265ce3d0d96134afaa
이용준 <abc157898@gmail.com>
2025-10-15 12:08:11 +0900

    Cal.1lv_반복 기능 및 탈출

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,77 @@
-//TIP 코드를 <b>실행</b>하려면 <shortcut actionId="Run"/>을(를) 누르거나
-// 에디터 여백에 있는 <icon src="AllIcons.Actions.Execute"/> 아이콘을 클릭하세요.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP 캐럿을 강조 표시된 텍스트에 놓고 <shortcut actionId="ShowIntentionActions"/>을(를) 누르면
-        // IntelliJ IDEA이(가) 수정을 제안하는 것을 확인할 수 있습니다.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP <shortcut actionId="Debug"/>을(를) 눌러 코드 디버그를 시작하세요. 1개의 <icon src="AllIcons.Debugger.Db_set_breakpoint"/> 중단점을 설정해 드렸습니다
-            // 언제든 <shortcut actionId="ToggleLineBreakpoint"/>을(를) 눌러 중단점을 더 추가할 수 있습니다.
-            System.out.println("i = " + i);
+        //계산기 기능을 구현할 콘솔창을 띄우기
+        Scanner scan = new Scanner(System.in);
+        //시스템에 입력을 할 수 있는 Scanner라는 객체를 새로 만들어서 scan 이라는 변수에 담겠다.
+
+        while (true) {
+            System.out.print("첫 번째 숫자 입력: ");
+            int num1 = scan.nextInt();
+
+            System.out.print("기호를 입력: ");
+            char gh = scan.next().charAt(0);
+            //문자열이 아닌 문자이기에 char을 사용, 이후 문자열의 ()번째를 추출하는 객체인 charAt()를 사용.
+
+            System.out.print("두 번째 숫자 입력: ");
+            int num2 = scan.nextInt();
+
+            int result = 0;
+
+            //콘솔을 통해 입력된 숫자가 실제 계산되기 위한 기능 구현(switch.ver)
+            switch (gh) {
+                case '+':
+                    result = num1 + num2;break;
+                case '-':
+                    result = num1 - num2;break;
+                case '*':
+                    result = num1 * num2;break;
+                case '/':
+                    if (num2 == 0) {
+                        System.out.println("분모에는 0이 들어갈 수 없습니다.");
+                    }
+                    result = num1 / num2;break;
+                default:
+                    System.out.println("잘못된 연산자 입니다.");
+            }
+
+            System.out.println("결과: " + result);
+
+            System.out.println("더 계산하시겠습니까? 원하지 않을 경우 입력: [exit]");
+            String answer = scan.next();
+
+            if (answer.equals("exit")) {
+                System.out.println("종료하겠습니다");
+                break;
+            } else {
+                continue;
+            }
         }
     }
 }
+
+
+//        String tex = scan.next();
+//        if(tex.equals("exit")) {
+//            System.out.println("종료하겠습니다.");
+//            break;
+//        }
+//        int num1 = Integer.parseInt(tex);
+
+//콘솔을 통해 입력된 숫자가 실제 계산되기 위한 기능 구현(if.ver)
+//        if (gh == '+') {
+//            result = num1 + num2;
+//        } else if (gh == '-') {
+//            result = num1 - num2;
+//        } else if (gh == '*') {
+//            result = num1 * num2;
+//        } else if (gh == '/') {
+//            if (num2 == 0) {
+//                System.out.println("나눗셈에 분모가 0이될 수는 없습니다.");
+//                return;
+//            } else {
+//                result = num1 / num2;
+//            }
+//        }
