78726bd1929bcf0231c6a2e43d9edbabcf9b1fdc
이용준 <abc157898@gmail.com>
2025-10-16 13:37:32 +0900

    2lv 콘솔 표시(완)

--- src/Calculator.java
+++ src/Calculator.java
@@ -6,41 +6,45 @@ public class Calculator {
 
     public Integer resultNum(int first, int second, char operator) {
         int result = 0;
-//         1)operator의 사칙연산의 부호가 무엇인지 판단을 한다. 조건문을 통해
-//         1.만약에 operator가 '+'가 들어간다면
+         //1)operator의 사칙연산의 부호가 무엇인지 판단을 한다. 조건문을 통해
+         //만약에 operator가 '+'가 들어간다면
         if (operator == '+') {
             // first + second 나온 값을 변수 result에 담는다.
             result = first + second;
-            // 3. result값을 반환한다.
         } else if (operator == '-') {
             result = first - second;
         } else if (operator == '*') {
             result = first * second;
         } else if (operator == '/') {
+        // second값이 0이 들어갈 경우 아래 내용을 프린트
+        // 이후 result값을 null로 반환
             if (second == 0){
                 System.out.println("0인 분모를 나눌 수 없습니다.");
                 return null;
             }
             result = first / second;
         } else {
+        // 외 다른 문자를 썼을 경우 아래와같이 표기 후 result값은 null로 변환
             System.out.println("잘못된 연산 기호 입니다.");
             return null;
         }
+        //계산된 result값을 resultList 배열에 저장
+        //계산된 result값을 반환
         resultList.add(result);
         return result;
     }
-    // 저장된 결과 값을 배열 형태로 불러오는 메소드
+    // 저장된 결과 값을 배열 형태로 불러오는 메서드
     ArrayList<Integer> getResultList() {
         return resultList;
     }
 
-    // 저장된 배열 속에서 수정하는 메소드 (0부터시작하는 순서, 바꿔들어갈 숫자)
+    // 저장된 배열 속에서 수정하는 메서드 (0부터시작하는 순서, 바꿔들어갈 숫자)
     void setResultList(int index, int newNum) {
         this.resultList.set(index, newNum);
     }
 
-    // 저장된 배열 속에서 삭제시키는 메소드 (순서 입력(0부터시작))
-    void removeResult(int index) {
-        this.resultList.remove(index);
+    // 저장된 배열 속에서 삭제시키는 메서드 (순서 입력(0부터시작))
+    void removeResult() {
+        this.resultList.remove(0);
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -10,16 +10,16 @@ public class Main {
         Scanner scan = new Scanner(System.in);
         //시스템에 입력을 할 수 있는 Scanner라는 객체를 새로 만들어서 scan 이라는 변수에 담겠다.
 
-        System.out.print("첫 번째 숫자 입력: ");
+        System.out.print("첫 번째 숫자를 입력해주세요.: ");
         int num1 = scan.nextInt();    //입력된 첫번째 숫자를 저장
 
         //계산을 이어나가기 위해 부호 입력부터 반복하도록 while문 사용
         while (true) {      //반복문 내용이 true일 시 지속 반복
-            System.out.print("기호를 입력: ");
+            System.out.print("기호를 입력해주세요.: ");
             char gh = scan.next().charAt(0);
             //문자열이 아닌 문자이기에 char을 사용, 이후 문자열의 ()번째를 추출하는 객체인 charAt()를 사용.
 
-            System.out.print("두 번째 숫자 입력: ");
+            System.out.print("두 번째 숫자를 입력해주세요.: ");
             int num2 = scan.nextInt(); //입력된 두번째 숫자를 저장
 
             Integer result = calculator.resultNum(num1, num2, gh);
@@ -32,20 +32,55 @@ public class Main {
 
 
             System.out.println("결과: " + result);
-            // 결과 값을 보여주기
+            // 결과 값 출력
             System.out.println("저장된 결과 : " + resultList);
+            // 결과값을 배열 형태로 출력
 
             num1 = result;
+            // 계산에 이어가기 위한 결과값 첫번째 넣어주기
 
-            // 나온 결과 값을 다음 계산에 이어가기 위해 첫번째 숫자에 넣어주기
+            //이후 진행여부 확인
+            System.out.println("원하시는 진행숫자를 입력해 주세요.[]");
+            System.out.println("1.이어서 계산\n2.저장값 수정\n3.저장 값 제거\n4.종료");
+            int allAnswer = scan.nextInt();
 
-            // if문을 통해 while 구문에서 if조건문을 통해 exit 입력 시 종료할 수 있도록 break 기능 사용
-            System.out.println("종료를 원하시면 [exit]를 입력해주세요.");
-            String answer = scan.next();
+            //각 숫자 입력시 실행되는 기능
+            // 1번 이어서 계산하도록 continue
+            if (allAnswer == 1) {
+                continue;
+            }
 
-            if (answer.equals("exit")) {
-                System.out.println("종료하겠습니다");
-                break;
+            //
+            if (allAnswer == 2) {
+                System.out.println("몇번째 값을 수정하시겠습니까?(순서는 0부터시작입니다.)");
+                int index = scan.nextInt();
+
+
+                System.out.println("수정될 값을 입력해주세요");
+                int newnum = scan.nextInt();
+
+
+                calculator.setResultList(index, newnum);
+                System.out.println("수정된 저장값 = " + resultList);
+            }
+
+            if (allAnswer == 3) {
+                calculator.removeResult();
+                System.out.println("수정된 저장값 = " + resultList);
+            }
+
+
+
+
+            //종료를 위한 입력
+            if (allAnswer == 4) {
+                System.out.println("exit를 입력해주세요.");
+            // if문을 통해 while 구문에서 exit 입력 시 종료할 수 있도록 break 기능 사용
+                String answer = scan.next();
+                if (answer.equals("exit")) {
+                    System.out.println("종료하였습니다");
+                    break;
+                }
             }
         }
     }
