6f33337023677352ac71c427b504e524210d565d
이용준 <abc157898@gmail.com>
2025-10-20 19:16:43 +0900

    3Lv 도전 기능 람다,스트림 활용 조회 기능 추가

--- src/LV3/Calculator.java
+++ src/LV3/Calculator.java
@@ -1,30 +1,33 @@
 package LV3;
 
 import java.util.ArrayList;
+import java.util.stream.Collectors;
 
+//연산 결과를 저장하는 컬랙션 타입 필드 선언 및 생성 , Enum을 활용하기 위한 class Enum 참조
 public class Calculator extends Enum {
-    //연산 결과를 저장하는 컬랙션 타입 필드 선언 및 생성
-    private ArrayList<Integer> resultList = new ArrayList<>();
 
-    public Integer resultNum(int first, int second, char operator) {
-        int result = 0;
+    // 컬랙션 타입을 Double로 변경함에 따라 실수까지 받을 수 있게 설정
+    private ArrayList<Double> resultList = new ArrayList<>();
+
+    //독립적 제네릭 'D'를 활용하여 연산기능 수정
+    public <D extends Number> Double resultNum(D first, D second, char operator) {
+        double result = 0;
         //1)operator의 사칙연산의 부호가 무엇인지 판단을 한다. 조건문을 통해
         //만약에 operator가 '+'가 들어간다면
         if (operator == Operator.PLUS.getOperator()) {
             // first + second 나온 값을 변수 result에 담는다.
-            result = first + second;
+            result = first.doubleValue() + second.doubleValue();
         } else if (operator == Operator.MINUS.getOperator()) {
-            result = first - second;
+            result = first.doubleValue() - second.doubleValue();
         } else if (operator == Operator.MULTPLY.getOperator()) {
-            result = first * second;
+            result = first.doubleValue() * second.doubleValue();
         } else if (operator == Operator.DIVIDE.getOperator()) {
-            // second값이 0이 들어갈 경우 아래 내용을 프린트
-            // 이후 result값을 null로 반환
-            if (second == 0){
+            // second값이 0이 들어갈 경우 아래 내용을 프린트 이후 result값을 null로 반환
+            if (second.doubleValue() == 0){
                 System.out.println("0인 분모를 나눌 수 없습니다.");
                 return null;
             }
-            result = first / second;
+            result = first.doubleValue() / second.doubleValue();
         } else {
             // 외 다른 문자를 썼을 경우 아래와같이 표기 후 result값은 null로 변환
             System.out.println("잘못된 연산 기호 입니다.");
@@ -37,17 +40,25 @@ public class Calculator extends Enum {
         return result;
     }
     // 저장된 결과 값을 배열 형태로 불러오는 메서드
-    ArrayList<Integer> getResultList() {
+    ArrayList<Double> getResultList() {
         return resultList;
     }
 
     // 저장된 배열 속에서 수정하는 메서드 (0부터시작하는 순서, 바꿔들어갈 숫자)
-    void setResultList(int index, int newNum) {
-        this.resultList.set(index - 1, newNum);
+    void setResultList(int index, double newNum) {
+        this.resultList.set(index - 1, newNum); // 순서를 1을 넣으면 첫번째 숫자가 지정되도로 -1을 붙여주었다.
     }
 
     // 저장된 배열 속에서 첫번째 숫자를 삭제시키는 메서드 (순서 입력(0부터시작))
     void removeResult() {
         this.resultList.remove(0);
+    }
+
+    //저장된 배열 속 입력한 숫자값보다 큰 수들을 배열 후 출력시키기
+    public   ArrayList<Double> findList(double findNum){
+             return resultList.stream()   // 기존 resultList를 스트림 형태로 변환하여 데이터 흐름을 만든다.
+                     .filter(n -> n > findNum) // findNum 입력값 보다 큰 수만 필터링한다.(람다 활용)
+                     .sorted()  // 필터링된 값을 정렬시킨다.
+                     .collect(Collectors.toCollection(ArrayList::new)); //필터링된 데이터를 ArryList에 다시 담는다.
     }
 }
--- src/Lv2/Main.java
+++ src/Lv2/Main.java
@@ -1,5 +1,4 @@
-package LV3;
-
+package Lv2;
 import java.util.ArrayList;
 import java.util.InputMismatchException;
 import java.util.Scanner;
@@ -14,30 +13,26 @@ public class Main {
         //시스템에 입력을 할 수 있는 Scanner라는 객체를 새로 만들어서 scan 이라는 변수에 담겠다.
 
         System.out.print("첫 번째 숫자를 입력해주세요.: ");
-        double num1 = scan.nextDouble();//입력된 첫번째 숫자를 저장
+        int num1 = scan.nextInt();//입력된 첫번째 숫자를 저장
 
         //계산을 이어나가기 위한 부호 입력부터 반복하도록 while문 사용
         while (true) {      //반복문 내용이 true일 시 지속 반복
             System.out.print("기호를 입력해주세요.: ");
             char gh = scan.next().charAt(0);
             //문자열이 아닌 문자이기에 char을 사용, 이후 문자열의 ()번째를 추출하는 객체인 charAt()를 사용.
-            double num2 = 0;
-
-            //try-catch문 통해 숫자 외 다른문자 입력 시 오류를 잡고 다시 입력하도록 설정
+            int num2 = 0;
             try {
                 System.out.print("두 번째 숫자를 입력해주세요.: ");
-                num2 = scan.nextDouble(); //입력된 두번째 숫자를 저장
+                num2 = scan.nextInt(); //입력된 두번째 숫자를 저장
             }catch (InputMismatchException e){
                 System.out.println("숫자를 다시 입력해주세요");
                 scan.next();
-                continue; // 숫자 잘못 입력 시 다음 문구와 함께 넘어가 다시 기호부터 설정
+                continue;
             }
+            Integer result = calculator.resultNum(num1, num2, gh);
+            ArrayList<Integer> resultList = calculator.getResultList();
 
-            //결과 값을 result에 저장, 결과 값 배열을 resultList에 저장
-            Double result = calculator.resultNum(num1, num2, gh);
-            ArrayList<Double> resultList = calculator.getResultList();
 
-            //결과 값이 null이 나올경우 건너뛰고 다시 돌아가도록 설정
             if (result == null) {
                 continue;
             }
@@ -47,12 +42,12 @@ public class Main {
             System.out.println("저장된 결과 : " + resultList);
             // 결과값을 배열 형태로 출력
 
-            // 계산에 이어가기 위한 결과값 첫 숫자에 넣어주기
             num1 = result;
+            // 계산에 이어가기 위한 결과값 첫번째 넣어주기
 
             //이후 진행여부 확인
             System.out.println("원하시는 진행숫자를 입력해 주세요.[]");
-            System.out.println("1.이어서 계산\n2.저장값 수정\n3.첫번째 저장 값 제거\n4.숫자 필터링\n5.종료");
+            System.out.println("1.이어서 계산\n2.저장값 수정\n3.저장 값 제거\n4.종료");
             int allAnswer = scan.nextInt();
 
             //각 숫자 입력시 실행되는 기능
@@ -61,48 +56,39 @@ public class Main {
                 continue;
             }
 
-            // 2번 결과 값 배열 내 숫자 수정하는 기능
+            //
             if (allAnswer == 2) {
                 int index;
 
                 while (true) {
                     System.out.println("몇번째 값을 수정하시겠습니까?");
-                    index = scan.nextInt(); //수정할 n번째를 입력
+                    index = scan.nextInt();
 
-                    if (index < 1) {   //index는 0부터 시작이기에 아래 값보다 설정될 경우 오류가 나도록 설정
+                    if (index < 1) {
                         System.out.println("잘못된 순서값입니다. 다시 입력해주세요");
                     } else {
-                        break;  //
+                        break;
                     }
                 }
 
 
                 System.out.println("수정될 값을 입력해주세요");
-                double newnum = scan.nextInt();
+                int newnum = scan.nextInt();
 
                 calculator.setResultList(index, newnum);
 
                 System.out.println("수정된 저장값 = " + resultList);
-                System.out.println("이어서 계산 하겠습니다.");
             }
 
-            //첫번째 저장 값을 지우는 remove기능 제거 설정
+
             if (allAnswer == 3) {
                 calculator.removeResult();
                 System.out.println("수정된 저장값 = " + resultList);
             }
 
-            //스트림 및 람다를 이용한 정한 숫자 보다 큰 값 조회하기
-            if (allAnswer == 4) {
-                System.out.println("숫자를 입력해주세요. 다음 입력한 숫자보다 큰 숫자들을 조회합니다.");
-                double num3 = scan.nextDouble();
-                ArrayList<Double> filtering = calculator.findList(num3);
-                System.out.println( num3 + "보다 큰값 :  " + filtering);
-            }
-
 
             //종료를 위한 입력
-            if (allAnswer == 5) {
+            if (allAnswer == 4) {
                 System.out.println("exit를 입력해주세요.");
                 // if문을 통해 while 구문에서 exit 입력 시 종료할 수 있도록 break 기능 사용
                 String answer = scan.next();
