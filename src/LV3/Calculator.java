b6800e25b1b95c1ecde31bec504d238bdbe6ec84
이용준 <abc157898@gmail.com>
2025-10-21 11:21:45 +0900

    3Lv 최종 수정 - 정렬

--- src/LV3/Calculator.java
+++ src/LV3/Calculator.java
@@ -23,7 +23,7 @@ public class Calculator {
             result = first.doubleValue() * second.doubleValue();
         } else if (operator == Operator.DIVIDE.getOperator()) {
             // second값이 0이 들어갈 경우 아래 내용을 프린트 이후 result값을 null로 반환
-            if (second.doubleValue() == 0){
+            if (second.doubleValue() == 0) {
                 System.out.println("0인 분모를 나눌 수 없습니다.");
                 return null;
             }
@@ -39,6 +39,7 @@ public class Calculator {
         resultList.add(result);
         return result;
     }
+
     // 저장된 결과 값을 배열 형태로 불러오는 메서드
     ArrayList<Double> getResultList() {
         return resultList;
@@ -55,10 +56,10 @@ public class Calculator {
     }
 
     //저장된 배열 속 입력한 숫자값보다 큰 수들을 배열 후 출력시키기
-    public   ArrayList<Double> findList(double findNum){
-             return resultList.stream()   // 기존 resultList를 스트림 형태로 변환하여 데이터 흐름을 만든다.
-                     .filter(n -> n > findNum) // findNum 입력값 보다 큰 수만 필터링한다.(람다 활용)
-                     .sorted()  // 필터링된 값을 정렬시킨다.
-                     .collect(Collectors.toCollection(ArrayList::new)); //필터링된 데이터를 ArryList에 다시 담는다.
+    public ArrayList<Double> findList(double findNum) {
+        return resultList.stream()   // 기존 resultList를 스트림 형태로 변환하여 데이터 흐름을 만든다.
+                .filter(n -> n > findNum) // findNum 입력값 보다 큰 수만 필터링한다.(람다 활용)
+                .sorted()  // 필터링된 값을 정렬시킨다.
+                .collect(Collectors.toCollection(ArrayList::new)); //필터링된 데이터를 ArryList에 다시 담는다.
     }
 }
